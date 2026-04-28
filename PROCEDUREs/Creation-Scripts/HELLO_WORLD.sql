c67098d36f019be5307efbf2366aa55b17f49732
berkeoz <berkeoz>
2026-04-28 16:01:18 -0700

    line 8

--- PROCEDUREs/Creation-Scripts/HELLO_WORLD.sql
+++ PROCEDUREs/Creation-Scripts/HELLO_WORLD.sql
@@ -5,7 +5,7 @@ BEGIN
  --Line 15:34
     --Test4
     --TEST2
-    --UPDATED_BY_DEV1 ---2 -- Line 8 15:05 --Line 8  15:07 --Line 8  15:15
+    --UPDATED_BY_DEV1 ---2 -- Line 8 15:05 --Line 8  15:07 --Line 8  15:15 -- Line8
 
     --UPDATED_BY_DEV2 - 2026.04.24 Change on Second user
 
@@ -13,5 +13,6 @@ BEGIN
 
     ---2312131
 END;
+/
 /
 
